b04c828243128f17c50d2b44524778fbe1d53cda
TomCom1234 <tomcom12344@gmail.com>
2024-03-07 14:25:40 +0000

    Renamed file and made variables be instantiated in the file

--- src/php/triggers/gform_after_submission_40.php
+++ src/php/triggers/gform_after_submission_40.php
@@ -3,23 +3,20 @@
  * Actions initiated by submission of Work Completed report
  * */
 
-add_action( 'gform_after_submission_40', 'sb_gform_after_submission_50', 10, 2 );
-function sb_gform_after_submission_50( $entry, $form ) {
-	BS_Log::info( 'called /triggers/gform_50_submission_ops_work_completed.php' );
+add_action( 'gform_after_submission_40', 'copy_project_details_old_to_new', 10, 2 );
+function copy_project_details_old_to_new( $entry, $form ) {
+	BS_Log::info( 'called /triggers/gform_after_submission_40.php' );
 
 	/**
 	 * Get Variables from entry
 	 */
 	global $variables_50_ops_work_completed;
-	include( 'get-entry-variables/variables_form_id_140_utilities_trigger_action.php' );
-
-	BS_Log::info( '$variables_140_util_trigger_actions: ' );
-	BS_Log::info( $variables_140_util_trigger_actions );
-
+	$id     = rgar( $entry, 'id' );
+	$action = rgar( $entry, '1' );
 	/*
 	 * Call scripts to process selected action
 	 * */
-	switch ( $variables_140_util_trigger_actions['action'] ) {
+	switch ( $action ) {
 		case 1:
 			BS_Log::info( 'Task: Copy Project Details Old To New' );
 			include BS_NAME_PLUGIN_DIR . 'triggers/actions/copy_project_old_to_new.php';
