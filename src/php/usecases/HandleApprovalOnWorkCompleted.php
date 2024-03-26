718b3bc8bfd50386d5f66e4cc87757299287d88f
Jayd Page <jayd@maxcutsoftware.com>
2024-03-26 10:39:00 +0200

    Add logging and refactor HandleApprovalOnWorkCompleted

--- src/php/usecases/HandleApprovalOnWorkCompleted.php
+++ src/php/usecases/HandleApprovalOnWorkCompleted.php
@@ -24,7 +24,6 @@ class HandleApprovalOnWorkCompleted {
 	 * @return void
 	 */
 	function update_client_balances( $step_id, $entry_id, $form_id, $status ) {
-		// TODO: why are we not updating the balance adjustments (child entries) here too? (form 151)
 		if ( $step_id != '52' ) {
 			return;
 		}
@@ -32,13 +31,33 @@ class HandleApprovalOnWorkCompleted {
 		SMPLFY_Log::info( "Approval step completed for entry $entry_id with status: $status" );
 
 		$workCompletedEntry = $this->workCompletedRepository->get_one_by_id( $entry_id );
+
+		$this->update_admin_client_remaining_balance( $status, $workCompletedEntry );
+		$this->update_client_closing_balance( $workCompletedEntry );
+	}
+
+	/**
+	 * @param $status
+	 * @param  WorkCompletedEntity  $workCompletedEntry
+	 *
+	 * @return void
+	 */
+	private function update_admin_client_remaining_balance( $status, WorkCompletedEntity $workCompletedEntry ): void {
+		// TODO: why are we not updating the balance adjustments (child entries) here too? (form 151)
+		$organizationName   = $workCompletedEntry->organisationName;
 		$adminClientBalance = $this->adminClientBalancesRepository->get_one_by_client_user_id( $workCompletedEntry->clientUserId );
 
-		$purchasedHours   = convert_to_float( $workCompletedEntry->hoursPurchased );
-		$organizationName = $workCompletedEntry->organisationName;
+		if ( empty( $adminClientBalance ) ) {
+			SMPLFY_Log::error( "Failed to update client remaining balance: No admin client balance found for client user id: $workCompletedEntry->clientUserId" );
+
+			return;
+		}
+
+		SMPLFY_Log::info( "Updating client balances after work completed ($status) for $organizationName ($workCompletedEntry->clientUserId): ", $workCompletedEntry );
+
+		$purchasedHours = convert_to_float( $workCompletedEntry->hoursPurchased );
 
 		if ( $status == 'approved' ) {
-			SMPLFY_Log::info( "Updating client balance after work completed APPROVAL for $organizationName ($workCompletedEntry->clientUserId): ", $workCompletedEntry );
 
 			$hoursConsumed = convert_to_float( $workCompletedEntry->hoursSpent );
 			$hoursBalance  = convert_to_float( $adminClientBalance->hoursRemaining );
@@ -48,10 +67,9 @@ class HandleApprovalOnWorkCompleted {
 			$adminClientBalance->hoursRemaining = $hoursNewBalance;
 			$this->adminClientBalancesRepository->update( $adminClientBalance );
 
-			SMPLFY_Log::info( "Number of hours pending approval updated from $hoursBalance to $hoursNewBalance for $organizationName" );
+			SMPLFY_Log::info( "Number of hours remaining updated from $hoursBalance to $hoursNewBalance for $organizationName" );
 
 		} elseif ( $status == 'rejected' ) {
-			SMPLFY_Log::info( "Updating client balance after work completed REJECTION for $organizationName ($workCompletedEntry->clientUserId): ", $workCompletedEntry );
 
 			$hoursPending  = convert_to_float( $adminClientBalance->hoursRemainingPendingApproval );
 			$hoursConsumed = convert_to_float( $adminClientBalance->hoursRemaining );
@@ -61,15 +79,24 @@ class HandleApprovalOnWorkCompleted {
 			$adminClientBalance->hoursRemainingPendingApproval = $hoursNewBalance;
 			$this->adminClientBalancesRepository->update( $adminClientBalance );
 
-			SMPLFY_Log::info( "Number of hours pending approval updated from $hoursPending to $hoursNewBalance for $organizationName" );
+			SMPLFY_Log::info( "Number of hours remaining pending approval updated $hoursPending to $hoursNewBalance for $organizationName" );
 		}
-
-		$this->update_customer_closing_balance( $workCompletedEntry );
 	}
 
-	function update_customer_closing_balance( WorkCompletedEntity $workCompletedEntry ) {
+	/**
+	 * @param  WorkCompletedEntity  $workCompletedEntry
+	 *
+	 * @return void
+	 */
+	private function update_client_closing_balance( WorkCompletedEntity $workCompletedEntry ) {
 		// TODO: logging
 		$clientBalance = $this->clientBalanceRepository->get_one_by_client_user_id( $workCompletedEntry->clientUserId );
+
+		if ( empty( $adminClientBalance ) ) {
+			SMPLFY_Log::error( "Failed to update client closing balance: No client balance found for client user id: $workCompletedEntry->clientUserId" );
+
+			return;
+		}
 
 		$hoursBalance   = convert_to_float( $clientBalance->hours );
 		$hoursPurchased = convert_to_float( $workCompletedEntry->hoursPurchased );
