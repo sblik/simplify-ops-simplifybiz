9108c63f6c48be033d354c945d50f588a0303d3f
TomCom1234 <tomcom12344@gmail.com>
2025-10-02 10:40:10 +0100

    Use correct log

--- src/php/usecases/WorkReportApproved.php
+++ src/php/usecases/WorkReportApproved.php
@@ -18,9 +18,9 @@ class WorkReportApproved
      */
     public function handle_workflow_approved($entry, $assignee, $new_status, $form)
     {
-        BS_Log::info("handle_workflow_approved triggered: ", $new_status);
+        SMPLFY_Log::info("handle_workflow_approved triggered: ", $new_status);
             if ( $new_status == 'approved' ) {
-                BS_Log::info("IN if new status is approved");
+                SMPLFY_Log::info("IN if new status is approved");
                wp_redirect("https://ops.simplifybiz.com/manager-dashboard/manager-tools/approvals/");
                die();
             }
