71f79c4204b410196493e2b26b83f0ffda8903c6
TomCom1234 <tomcom12344@gmail.com>
2025-10-30 11:20:46 +0000

    Logic to redirect to specific pages after approval of certain workflow steps

--- src/php/usecases/WorkReportApproved.php
+++ src/php/usecases/WorkReportApproved.php
@@ -19,9 +19,24 @@ class WorkReportApproved
     public function handle_workflow_approved($entry, $assignee, $new_status, $form)
     {
         SMPLFY_Log::info("handle_workflow_approved triggered: ", $new_status);
+
+        $formID = $form['id'];
+        SMPLFY_Log::info("Form ID: ", $formID);
             if ( $new_status == 'approved' ) {
                 SMPLFY_Log::info("IN if new status is approved");
-               wp_redirect("https://ops.simplifybiz.com/inbox");
+                if($formID == 50){
+                    $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-approvals/";
+                } elseif($formID == 181){
+                    $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-internships/";
+                }
+                elseif($formID == 172){
+                   $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-task-requests/";
+                }
+                if(!empty($redirectURL)){
+                    SMPLFY_Log::info("Redirect URL: ", $redirectURL);
+                    wp_redirect("https://ops.simplifybiz.com/inbox/inbox-approvals/");
+                }
+
                die();
             }
     }
