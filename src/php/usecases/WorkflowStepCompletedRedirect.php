c1bac2f8ade5d4d76a430e4f2200d4a069fd8d20
TomCom1234 <tomcom12344@gmail.com>
2025-10-30 12:08:45 +0000

    only redirect if certain user

--- src/php/usecases/WorkflowStepCompletedRedirect.php
+++ src/php/usecases/WorkflowStepCompletedRedirect.php
@@ -21,21 +21,24 @@ class WorkflowStepCompletedRedirect
         SMPLFY_Log::info("handle_workflow_step_completed triggered: ", $status);
 
         SMPLFY_Log::info("Form ID: ", $formID);
-            if($formID == 50){
+        $currentUser = get_user_by('ID', get_current_user_id());
+        $isManager   = UserActions::does_user_have_role($currentUser, 'manager');
+        if ($isManager) {
+            if ($formID == 50) {
                 $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-approvals/";
-            } elseif($formID == 181){
+            } elseif ($formID == 181) {
                 $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-internships/";
-            }
-            elseif($formID == 172 || $formID == 170){
+            } elseif ($formID == 172 || $formID == 170) {
                 $redirectURL = "https://ops.simplifybiz.com/inbox/inbox-task-requests/";
             }
-            if(!empty($redirectURL)){
+            if (!empty($redirectURL)) {
                 SMPLFY_Log::info("Redirect URL: ", $redirectURL);
                 wp_redirect("https://ops.simplifybiz.com/inbox/inbox-approvals/");
-            }else{
+            } else {
                 wp_redirect("https://ops.simplifybiz.com/inbox/");
             }
 
-        exit;
+            exit;
+        }
     }
 }
