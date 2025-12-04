2d71ff0c2ca8d3eb8367d057997986e1c4d35b52
andrenellin <28567813+andrenellin@users.noreply.github.com>
2025-12-04 13:16:41 -0700

    Update smplfy.js

--- src/js/smplfy.js
+++ src/js/smplfy.js
@@ -13,11 +13,5 @@ jQuery(document).ready(function ($) {
 
     $(".datepicker").attr("autocomplete", "off");
 
-    $('.gv-read-only input, .gv-read-only textarea').attr('readonly', true);
-    
-    // Prevent select changes but keep value submittable
-    $('.gv-read-only select').on('mousedown', function(e) {
-        e.preventDefault();
-    });
 
 });
