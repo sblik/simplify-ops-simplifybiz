c7021a9e6fbd2f62f782dacdef13570186dfc988
TomCom1234 <tomcom12344@gmail.com>
2025-12-10 16:21:53 +0000

    Include dependencies

--- src/php/DependencyFactory.php
+++ src/php/DependencyFactory.php
@@ -3,48 +3,52 @@
 /**
  * A factory class responsible for creating and initializing all dependencies used in the plugin
  */
-class DependencyFactory {
+class DependencyFactory
+{
 
-	/**
-	 * Create and initialize all dependencies
-	 *
-	 * @return void
-	 */
-	static function create_plugin_dependencies() {
-		$gravityFormsWrapper = new SMPLFY_GravityFormsApiWrapper();
+    /**
+     * Create and initialize all dependencies
+     *
+     * @return void
+     */
+    static function create_plugin_dependencies()
+    {
+        $gravityFormsWrapper = new SMPLFY_GravityFormsApiWrapper();
 
-		// Repositories
-		$workCompletedRepository           = new WorkCompletedRepository( $gravityFormsWrapper );
-		$clientBalanceRepository           = new ClientBalanceRepository( $gravityFormsWrapper );
-		$clientBalanceAdjustmentRepository = new ClientBalanceAdjustmentRepository( $gravityFormsWrapper );
+        // Repositories
+        $workCompletedRepository           = new WorkCompletedRepository($gravityFormsWrapper);
+        $clientBalanceRepository           = new ClientBalanceRepository($gravityFormsWrapper);
+        $clientBalanceAdjustmentRepository = new ClientBalanceAdjustmentRepository($gravityFormsWrapper);
+        $taskRepository                    = new TaskRepository($gravityFormsWrapper);
 
-		// Use cases
-		$updateHoursWorked             = new UpdateHoursWorked( $workCompletedRepository, $clientBalanceRepository );
-		$workReportCompleted           = new WorkReportSubmitted( $workCompletedRepository, $clientBalanceRepository );
-		$handleApprovalOnWorkCompleted = new HandleApprovalOnWorkCompleted( $workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository );
-		$recalculateClientBalance      = new RecalculateClientBalance( $workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository );
-		$menuLoaded                    = new MenuLoaded( $clientBalanceRepository );
-		$addUserContactMethod          = new AddUserContactMethod();
-		$userLogin          = new UserLogin();
-		$workReportApproved          = new WorkReportApproved();
+        // Use cases
+        $updateHoursWorked             = new UpdateHoursWorked($workCompletedRepository, $clientBalanceRepository);
+        $workReportCompleted           = new WorkReportSubmitted($workCompletedRepository, $clientBalanceRepository);
+        $handleApprovalOnWorkCompleted = new HandleApprovalOnWorkCompleted($workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository);
+        $recalculateClientBalance      = new RecalculateClientBalance($workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository);
+        $menuLoaded                    = new MenuLoaded($clientBalanceRepository);
+        $addUserContactMethod          = new AddUserContactMethod();
+        $userLogin                     = new UserLogin();
+        $workReportApproved            = new WorkReportApproved();
+        $taskStatus                    = new TaskStatus($taskRepository);
 
-		// Handlers
-		$updateClientBalancesHandler = new UpdateClientBalancesHandler( $workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository );
+        // Handlers
+        $updateClientBalancesHandler = new UpdateClientBalancesHandler($workCompletedRepository, $clientBalanceRepository, $clientBalanceAdjustmentRepository);
 
         // Billable hours reports
-        $billableHoursReport = new BillableHoursReport($workCompletedRepository);
-        $googleChatWebhookUrl = defined('SMPLFY_GOOGLE_CHAT_WEBHOOK_URL') ? SMPLFY_GOOGLE_CHAT_WEBHOOK_URL : '';
+        $billableHoursReport       = new BillableHoursReport($workCompletedRepository);
+        $googleChatWebhookUrl      = defined('SMPLFY_GOOGLE_CHAT_WEBHOOK_URL') ? SMPLFY_GOOGLE_CHAT_WEBHOOK_URL : '';
         $billableHoursNotification = new BillableHoursNotification($billableHoursReport, $googleChatWebhookUrl);
-        $adminDashboard = new AdminDashboard($billableHoursReport);
+        $adminDashboard            = new AdminDashboard($billableHoursReport);
 
-		// Adapters
-		new GravityFormsAdapter( $updateHoursWorked, $workReportCompleted, $recalculateClientBalance );
-		new GravityFlowAdapter( $handleApprovalOnWorkCompleted, $workReportApproved );
-		new WordPressAdapter( $addUserContactMethod, $menuLoaded, $userLogin, $adminDashboard, $billableHoursNotification );
-		new MemberpressAdapter( $userLogin);
+        // Adapters
+        new GravityFormsAdapter($updateHoursWorked, $workReportCompleted, $recalculateClientBalance);
+        new GravityFlowAdapter($handleApprovalOnWorkCompleted, $workReportApproved, $taskStatus);
+        new WordPressAdapter($addUserContactMethod, $menuLoaded, $userLogin, $adminDashboard, $billableHoursNotification);
+        new MemberpressAdapter($userLogin);
 
 
-		// Api
-		new ControllerFactory( $updateClientBalancesHandler );
-	}
+        // Api
+        new ControllerFactory($updateClientBalancesHandler);
+    }
 }
